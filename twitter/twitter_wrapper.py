51f8e37aee8a776f12a81d486f4009bd20a11c74
Tominous <18189490+Tominous@users.noreply.github.com>
2020-04-06 15:50:00 +0100

    Update twitter_wrapper.py

--- twitter/twitter_wrapper.py
+++ twitter/twitter_wrapper.py
@@ -12,7 +12,7 @@ class TwitterApi:
 
 	def on_status_change(self, userIds, process):
 		params = {
-			# 'follow':'2449437698'
+			 'follow':'2449437698'
 			'follow': ",".join(userIds)
 		}
 		url = STREAM_FILTER_STATUSES
@@ -23,7 +23,7 @@ class TwitterApi:
 			for line in response.iter_lines():
 				if line:
 					jsonifiedResponse = json.loads(line)
-					# print(json.dumps(jsonifiedResponse, indent = 4))
+					 print(json.dumps(jsonifiedResponse, indent = 4))
 					if jsonifiedResponse["user"]["id_str"] in userIds and jsonifiedResponse["in_reply_to_status_id"] == None and jsonifiedResponse["in_reply_to_user_id"] == None:
 						tweet = Tweet(jsonifiedResponse["text"], jsonifiedResponse["user"]["id_str"])
 						process(tweet)
